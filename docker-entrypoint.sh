78d05c4557502c8cb343ad97128e62748118569b
wqy <1442963504@qq.com>
2019-07-25 00:50:35 +0800

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+CRT="/cert/"${CERT_PREFIX}"cert.crt"
+KEY="/cert/"${CERT_PREFIX}"cert.key"
+if [ $CERT ];then
+	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
+		acme.sh --issue --fullchain-file $CRT --key-file $KEY --challenge-alias $URL --dns dns_ali -d $CERT
+	fi
+fi
+trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
+crond && while true; do sleep 1; done;
