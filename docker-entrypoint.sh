da15507ad82bf0b718ee465ce125b333c7da1863
wqy <annata@users.noreply.github.com>
2021-08-19 12:58:36 +0800

    默认ca改回为letsencrypt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ CRT="/cert/"${CERT_PREFIX}"cert.crt"
 KEY="/cert/"${CERT_PREFIX}"cert.key"
 if [ $CERT ];then
 	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
-		acme.sh --issue --fullchain-file $CRT --key-file $KEY --challenge-alias $URL --dns dns_ali -d $CERT
+		acme.sh --issue --fullchain-file $CRT --key-file $KEY --challenge-alias $URL --dns dns_ali -d $CERT --server letsencrypt
 	fi
 fi
 trap "echo stop && killall crond && exit 0" SIGTERM SIGINT 
