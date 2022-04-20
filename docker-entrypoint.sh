09b47eb73949e0764dc837e7f7d44091e2259033
wqy <annata@users.noreply.github.com>
2022-04-20 12:17:33 +0800

    使用zerossl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
+mkdir -p /cert
 CRT="/cert/"${CERT_PREFIX}"cert.crt"
 KEY="/cert/"${CERT_PREFIX}"cert.key"
 if [ $CERT ];then
 	if ! [[ $(acme.sh --list) =~ $CERT ]]; then
+		acme.sh  --register-account  -m spriest-rebates06@icloud.com --server zerossl
 		acme.sh --issue --fullchain-file $CRT --key-file $KEY --challenge-alias $URL --dns dns_ali -d $CERT
 	fi
 fi
